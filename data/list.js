120e348fdbe444b0a96655c56cfe8039eb0c3a41
wmahathre <41713069+wmahathre@users.noreply.github.com>
2018-09-25 16:55:39 -0400

    Add files via upload

---
+++ data/list.js
@@ -0,0 +1,8 @@
+var toDoArray = [
+    /*{
+      newInput: "first test"
+    }*/
+  ];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+module.exports = toDoArray;
